268872e7bff550b7f7381024f0a82290cffd5c7e
Michal Zygor <michal@winami.com>
2020-10-22 21:32:13 +0200

    Updated according to comment. Enough use boolean return instead of getter etc. Additional implemented limit parameter inside of constructor. Use main() method in GuessNumber to run.

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 
 public class GuessNumber {
     public static void main(String[] args) {
-        System.out.print("Cześć! Program zakończy działanie, kiedy podasz własciwą liczbę całkowitą :)");
-        Operations operations = new Operations();
+        System.out.println("Cześć! Program zakończy działanie, kiedy podasz własciwą liczbę całkowitą :)");
+        Operations operations = new Operations(100, 200, 3);
         Scanner scanner = new Scanner(System.in);
         do {
-            System.out.println(operations.getComment());
             System.out.print("Podaj liczbę: ");
-            operations.checkNumber(scanner.nextInt());
-        }while(!operations.isStatus());
-        System.out.println(operations.getComment());
+        }while(operations.checkNumber(scanner.nextInt()));
     }
 }
--- src/Operations.java
+++ src/Operations.java
@@ -1,37 +1,28 @@
 public class Operations {
-    private String comment;
-    private boolean status;
     final int minNumber;
     final int maxNumber;
     final int aliquotNumber;
 
-    public Operations() {
-        comment = "";
-        status = false;
-        minNumber = 100;
-        maxNumber = 200;
-        aliquotNumber = 3;
+    public Operations(int minNumber, int maxNumber, int aliquotNumber) {
+        this.minNumber = minNumber;
+        this.maxNumber = maxNumber;
+        this.aliquotNumber = aliquotNumber;
     }
 
-    public void checkNumber(int number) {
+    public boolean checkNumber(int number) {
         if(number >= minNumber && number <= maxNumber && number % aliquotNumber == 0){
-            comment = "Trafiłeś! Podana liczba jest podzielna przez " + aliquotNumber + " oraz należy do przedziału "
-                 + "liczb <" + minNumber + "," + maxNumber + ">";
-            status = true;
+            System.out.println("Trafiłeś! Podana liczba jest podzielna przez " + aliquotNumber + " oraz należy do przedziału "
+                 + "liczb <" + minNumber + "," + maxNumber + ">");
+            return false;
         } else if (number >= minNumber && number <= maxNumber){
-            comment = "Blisko. Niestety podana liczba nie jest podzielna przez " + aliquotNumber;
+            System.out.println("Blisko. Niestety podana liczba nie jest podzielna przez " + aliquotNumber);
+            return true;
         }else if(number < minNumber){
-            comment = "Podana liczba jest za mała";
+            System.out.println("Podana liczba jest za mała");
+            return true;
         }else {
-            comment = "Podana liczba jest za duża";
+            System.out.println("Podana liczba jest za duża");
+            return true;
         }
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public boolean isStatus() {
-        return status;
     }
 }
